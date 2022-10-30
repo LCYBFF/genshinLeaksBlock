a1e80ae4816564caadcf8a8740d97d60f8eca0d7
Laicy <240298098@qq.com>
2022-10-30 09:29:30 +0800

    Update genshinLeaksBlock.js
    V1.1.0
    
    更新关键词
    新增B站新旧版的替换

--- genshinLeaksBlock.js
+++ genshinLeaksBlock.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         B站与油管屏蔽原神内鬼爆料相关视频
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  祝原神爆料内鬼冚家富贵，雄性抵插棍，雌性烂臭閪
 // @author       凡云 - https://space.bilibili.com/3491267
 // @match        *://*.bilibili.com/*
@@ -14,19 +14,25 @@
  
 (function() {
     'use strict';
-    var fuckOffRecoString = /2\.9|3\.0|3\.1|3\.2|草神|爆料|草 神|白术|草元素|草反应|草系反应|草环境/
-    var fuckOffSearchString = /2\.9|3\.0|3\.1|3\.2|草神|爆料|草 神|草元素|草反应|草系反应|草环境/
+    var fuckOffRecoString = /2\.9|3\.0|3\.1|3\.2|草神|草 神|爆料|白术|草元素|草反应|草系反应|草雷|草环境|智慧/
+    var fuckOffSearchString = /2\.9|3\.0|3\.1|3\.2|草神|草 神|爆料|草元素|草反应|草系反应|草雷|草环境|智慧/
     var fuckOffYtbString = /2\.9|3\.0|3\.1|3\.2|dendro|leak|tighnari|草元素|草反应|草系反应|草环境/
  
     setInterval(() => {
-        // B站首页推荐栏
+        // B站首页推荐栏（旧版）
+        fuckOffBili(fuckOffRecoString, '.rcmd-box', '.video-card-reco', '.title', false)
+        // B站首页推荐栏（新版）
         fuckOffBili(fuckOffRecoString, '.recommend-container__2-line', '.bili-video-card', '.bili-video-card__info--tit', true)
-        // B站右侧推荐栏
+        // B站右侧推荐栏（旧版）
         fuckOffBili(fuckOffRecoString, '#reco_list', '.video-page-card', '.title', true)
-        // B站视频播放结束推荐列表
+        // B站右侧推荐栏（新版）
+        fuckOffBili(fuckOffRecoString, '#reco_list', '.video-page-card-small', '.title', true)
+        // B站视频播放结束推荐列表（新旧版）
         fuckOffBili(fuckOffRecoString, '.bpx-player-ending-related', '.bpx-player-ending-related-item', '.bpx-player-ending-related-item-title', true)
-        // B站搜索页
+        // B站搜索页（旧版）
         fuckOffBili(fuckOffSearchString, '#server-search-app .video-list', '.video-item', '.title', true)
+        // B站新版搜索页（新版）
+        fuckOffBili(fuckOffSearchString, '.search-page .video-list', '.video-list-item', '.bili-video-card__info--tit', true)
         // 油管首页
         fuckOffYtb(fuckOffYtbString, 'ytd-rich-grid-renderer', 'ytd-rich-item-renderer', 'yt-formatted-string', false)
         // 油管搜索页
@@ -41,7 +47,6 @@
                 let that = $(this)
                 let testString = fuckOffFormat(that.find(title).text())
                 if (fuckOffString.test(testString)) {
-                    // console.log(testString)
                     if (isRemove) {
                         that.remove()
                     } else {
